db6fcb7283c280426698e22036b7f002bb79df7f
Skekdog <47750815+Skekdog@users.noreply.github.com>
2026-02-07 21:45:19 +0000

    init

---
+++ LICENSE.md
@@ -0,0 +1,6 @@
+Copyright (c) 2026 Skekdog. All rights reserved.
+
+This software is the proprietary information of Skekdog.
+Unauthorized copying, modification, distribution, or use of this software,
+via any medium, is strictly prohibited unless prior written permission is obtained
+from the copyright holder.
---
+++ eslint.config.ts
@@ -0,0 +1,71 @@
+import js from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+	tseslint.configs.recommended,
+	{
+		files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+		plugins: {
+			js,
+			stylistic,
+			tseslint,
+		},
+		extends: ["js/recommended"],
+		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+		},
+
+		linterOptions: {
+			reportUnusedInlineConfigs: "warn",
+		},
+
+		rules: {
+			"no-duplicate-imports": "warn",
+			"no-template-curly-in-string": "warn",
+			"no-use-before-define": "warn",
+			"no-useless-assignment": "warn",
+			"eqeqeq": "warn",
+			"no-eval": "warn",
+			"no-implicit-globals": "warn",
+			"no-implied-eval": "warn",
+			"no-invalid-this": "warn",
+			"no-shadow": "warn",
+			"no-unused-expressions": "warn",
+			"yoda": "warn",
+			"no-restricted-syntax": ["error", "SwitchStatement"],
+			"no-constant-condition": "warn",
+
+			"stylistic/indent": ["warn", "tab"],
+			"stylistic/quotes": ["warn", "double", {
+				"avoidEscape": true,
+			}],
+			"stylistic/semi": ["error", "always"],
+
+			"@typescript-eslint/no-unused-vars": [
+				"warn",
+				{
+					"args": "all",
+					"argsIgnorePattern": "^_",
+					"caughtErrors": "all",
+					"caughtErrorsIgnorePattern": "^_",
+					"destructuredArrayIgnorePattern": "^_",
+					"varsIgnorePattern": "^_",
+					"ignoreRestSiblings": true
+				}
+			],
+			"@typescript-eslint/no-explicit-any": ["off"],
+			"no-unused-vars": ["off"],
+		},
+	},
+	{
+		files: ["./Source/Web/**/*.{js,mjs,cjs,ts,mts,cts}"],
+		languageOptions: {
+			globals: globals.browser
+		}
+	}
+]);
